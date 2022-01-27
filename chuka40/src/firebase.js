d04c1a4325fe19af6c149d6c470ae0c5274c7daf
KayodeOusi <kayodeolusi@gmail.com>
2022-01-27 19:05:01 +0100

    set up the environment and dependencies

---
+++ chuka40/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLfdT1ke5-lvm-y5hwejR6NkWXr2PIueo",
+  authDomain: "chuka-40.firebaseapp.com",
+  projectId: "chuka-40",
+  storageBucket: "chuka-40.appspot.com",
+  messagingSenderId: "473249674338",
+  appId: "1:473249674338:web:c08edfdcd78ff1db810d3b",
+  measurementId: "G-KT5KHMC1NV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+    
+export { db }; 
