bdd0a6abbba9f0e8e2dde4fdcca85953ab16d296
KayodeOusi <kayodeolusi@gmail.com>
2022-02-08 09:23:00 +0100

    ...

--- src/components/admin/AdminModalOptions.js
+++ src/components/admin/AdminModalOptions.js
@@ -1,6 +1,6 @@
 const AdminModalOptions = ({ name, onChange, ...rest}) => {
     return ( 
-            <form className="checkbox mt-3" onClick = { onChange }>
+            <form className="checkbox container mt-3" onClick = { onChange }>
                 <div className = "available">
                     <label htmlFor = { name }>{ name }</label>
                     <input { ...rest }  onChange={ onChange } type = "checkbox" name = { name } id = { name } value = { name } className = "the-checkbox"/>
